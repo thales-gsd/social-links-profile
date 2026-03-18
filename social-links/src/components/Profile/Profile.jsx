0c2573e582a5dfb8ef5f928a249f97765e86cdad
Thales Gregorio <137453849+thales-gsd@users.noreply.github.com>
2026-03-17 23:49:22 -0300

    fix image

--- social-links/src/components/Profile/Profile.jsx
+++ social-links/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import './Profile.css'
 export default function Profile() {
   return (
     <div className="profile">
-      <img src="/src/assets/avatar-thales.jpeg" alt="avatar" />
+      <img src="/avatar-thales.jpeg" alt="avatar" />
       <h1>Thales Dipa</h1>
       <p className="local">Limeira, Brazil</p>
     </div>
